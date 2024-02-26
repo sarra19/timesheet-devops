f5a1439394500141c739af8f074b6a1e87802264
sarra19 <sarra.benhamida@esprit.tn>
2024-02-26 11:13:08 +0100

    ajout commentaire 2

--- src/main/java/tn/esprit/spring/entities/User.java
+++ src/main/java/tn/esprit/spring/entities/User.java
@@ -18,7 +18,7 @@ import javax.persistence.TemporalType;
 @Table(name = "T_USER")
 public class User implements Serializable {
 	//commentaire
-
+	//test
 	private static final long serialVersionUID = 1L;
 
 	@Id
